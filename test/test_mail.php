e0107679beb2535f66eb46e3371377f8f37e39c2
forma2plus <forma2plus@bitbucket.org>
2018-01-03 19:43:10 +0100

    test exist

--- test/test_mail.php
+++ test/test_mail.php
@@ -1,30 +1,7 @@
 <?php
-require('../phpmailer/class.phpmailer.php');
-
-$mail = new PHPMailer();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth   = true;
-$mail->Port = 587; // Par défaut
-
-// Authentification
-$mail->Username = "antsiresy@gmail.com";
-$mail->Password = "2017tsiresy";
-
-// Expéditeur
-$mail->SetFrom('antsiresy@gmail.com', 'Nom Prénom');
-// Destinataire
-$mail->AddAddress('antsiresy2@gmail.com', 'Nom Prénom');
-// Objet
-$mail->Subject = 'Objet du message';
-
-// Votre message
-$mail->MsgHTML('Contenu du message en HTML');
-
-// Envoi du mail avec gestion des erreurs
-if(!$mail->Send()) {
-    echo 'Erreur : ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent !';
-} 
+if(function_exists('mail'))
+{
+    echo "Fonction existe";
+}
       
  ?>
