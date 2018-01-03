0230c99dd02d9ac97ef5a494758ebcfbad6a9c74
trajaonarison <trajaonarison@absys.fr>
2018-01-03 12:22:56 +0100

    More stuff

--- test/test_mail.php
+++ test/test_mail.php
@@ -6,5 +6,14 @@
      'Reply-To: antsiresy@gmail.com' . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
 
-     mail($to, $subject, $message, $headers);
+//      mail($to, $subject, $message, $headers);
+     
+     
+     $CR_Mail = TRUE;
+     
+     $CR_Mail = @mail ($to, $subject, $message, $headers);
+     
+     if ($CR_Mail === FALSE)   echo " ### CR_Mail=$CR_Mail - Erreur envoi mail <br> \n";
+     else                      echo " *** CR_Mail=$CR_Mail - Mail envoyé<br> \n";
+      
  ?>
