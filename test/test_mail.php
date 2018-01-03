0543acc4a7f8ed554a194e550775013df409c2c0
forma2plus <forma2plus@bitbucket.org>
2018-01-03 19:24:24 +0100

    More stuff

--- test/test_mail.php
+++ test/test_mail.php
@@ -1,10 +1,7 @@
 <?php
 require('../phpmailer/class.phpmailer.php');
-$mail = new PHPMailer();
 
-$mail             = new PHPMailer(); // defaults to using php "mail()"
-
-$mail->IsSendmail(); // telling the class to use SendMail transport
+$mail             = new PHPMailer();
 
 
 $mail->SetFrom('antsiresy@gmail.com', 'First Last');
@@ -14,11 +11,11 @@ $mail->AddReplyTo("antsiresy@gmail.com","First Last");
 $address = "antsiresy2@gmail.com";
 $mail->AddAddress($address, "John Doe");
 
-$mail->Subject    = "PHPMailer Test Subject via Sendmail, basic";
+$mail->Subject    = "PHPMailer Test Subject via mail(), basic";
 
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; 
 
-$mail->MsgHTML("<h1>Tet</h1>");
+$mail->MsgHTML("<h1>Test</h1>");
 
 
 if(!$mail->Send()) {
