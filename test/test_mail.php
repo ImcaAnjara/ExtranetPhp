bd699bbefccfad0b9899f346ee4c6a1205f67619
forma2plus <forma2plus@bitbucket.org>
2018-01-03 18:41:35 +0100

    more

--- test/test_mail.php
+++ test/test_mail.php
@@ -3,18 +3,32 @@
 require '../phpmailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
-$mail->setFrom('antsiresy@gmail.com', 'First Last');
-$mail->addReplyTo('antsiresy@gmail.com', 'First Last');
-$mail->addAddress('antsiresy2@gmail.com', 'John Doe');
-$mail->Subject = 'PHPMailer mail() test';
-// $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-$mail->Body = 'This is a plain-text message body';
-// $mail->addAttachment('images/phpmailer_mini.png');
 
-if (!$mail->send()) {
+$mail->IsSMTP();
+$mail->SMTPDebug = 1;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; // or 587
+$mail->IsHTML(true);
+$mail->Username = "antsiresy@gmail.com";
+$mail->Password = "2017tsiresy";
+
+$mail->setFrom('antsiresy@gmail.com', 'support');
+$mail->addAddress('antsiresy2@gmail.com', 'Uma');
+$mail->addReplyTo('antsiresy@gmail.com', 'Support');
+$mail->isHTML(true);
+
+$mail->Subject = 'Subject';
+$mail->Body = 'Body';
+$mail->AltBody = 'Dear Uma, Thank you for your interest.';
+
+
+if(!$mail->send()) {
+    echo "Opps! For some technical reasons we couldn't able to sent you an email. We will shortly get back to you with download details.";
     echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-    echo "Message sent!";
+    echo "Message has been sent";
 }
       
  ?>
