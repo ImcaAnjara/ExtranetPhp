8610f29aa41c1781d8444583113141860e4f90d6
forma2plus <forma2plus@bitbucket.org>
2018-01-03 19:20:08 +0100

    Add php mailer

--- test/test_mail.php
+++ test/test_mail.php
@@ -1,5 +1,30 @@
 <?php
+require('../phpmailer/class.phpmailer.php');
+$mail = new PHPMailer();
+
+$mail             = new PHPMailer(); // defaults to using php "mail()"
+
+$mail->IsSendmail(); // telling the class to use SendMail transport
 
 
+$mail->SetFrom('antsiresy@gmail.com', 'First Last');
+
+$mail->AddReplyTo("antsiresy@gmail.com","First Last");
+
+$address = "antsiresy2@gmail.com";
+$mail->AddAddress($address, "John Doe");
+
+$mail->Subject    = "PHPMailer Test Subject via Sendmail, basic";
+
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+$mail->MsgHTML("<h1>Tet</h1>");
+
+
+if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
       
  ?>
