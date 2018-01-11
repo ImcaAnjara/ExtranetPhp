7ad7563b28e8b2df0a871e3790c95a5043d1913b
forma2plus <forma2plus@bitbucket.org>
2018-01-11 06:16:40 +0100

    More stuff

--- note.php
+++ note.php
@@ -3,4 +3,15 @@ session_start();
 // session_unset ();
 $numtest=$_GET["numerodossiertest"];
 echo($_SESSION['note'.$numtest]);
+echo "<br>";
+$tps = $_SESSION["tpssec"];
+if($tps>=60){
+    $min = (int)$tps/60;
+    $sec = $tps%60;
+}else{
+    $min = 0;
+    $sec = $tps;
+}
+echo("temps passé: $min et $sec");
+
 ?>
--- traiter.php
+++ traiter.php
@@ -1,20 +1,26 @@
 <?php 
 session_start();
 
-if(!isset($_SESSION['note'.$numtest])){
-$_SESSION['note'.$numtest] = 0;
-}
+
 $numtest=$_POST["numerotest"];
 $nomtest=$_POST["testnom"];
 $num = $_POST["numquestion"];
 $temps= $_POST["time"];
 $_SESSION["time"] = $temps;
 $_SESSION['iteration'.$numtest] = $_SESSION['iteration'.$numtest] + 1;
+if(!isset($_SESSION['note'.$numtest])){
+    $_SESSION['note'.$numtest] = 0;
+}
 
 if($_POST["choix"]==$_SESSION["reponse"]){
     $_SESSION['note'.$numtest] = $_SESSION['note'.$numtest] +1 ;
 }
-if($num==50){
+$temps = explode(":",$_SESSION["time"]);
+$minutes = (int)$temps[0];
+$secondes = (int)$temps[1];
+
+$_SESSION["tpssec"] = (2 * 60 ) - (($minutes *60) + $secondes);
+if($num==10){
     header("Location: note.php?numerodossiertest=$numtest&nomtest=$nomtest");
 }
 else{    
