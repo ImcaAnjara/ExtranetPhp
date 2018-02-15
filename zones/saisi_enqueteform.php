89c75f29a3c3dd0bc6e2ad72ff602174eaa2920e
forma2plus <forma2plus@bitbucket.org>
2018-02-15 15:16:07 +0100

    More

--- zones/saisi_enqueteform.php
+++ zones/saisi_enqueteform.php
@@ -20,9 +20,29 @@
                     <div class="row">
 						<section>
                             <div class="col-md-6 col-md-offset-3">
-                            <h4>ENQUÊTE DE SATISFACTION :<?php echo($_GET["compteur"]);?> </h4>
+                            <h4>ENQUÊTE DE SATISFACTION : <?php echo($_GET["compteur"]);?> </h4>
                             </div>
                         </section>
+                        
+                        <section class="content-section bg-light" id="about">
+                              <div class="container text-left">
+                                <div class="row">
+                                  <div class="col-lg-10 mx-auto">
+                                  <label>Nom et prénom de l'apprenant :</label><br>
+                                  <label>Société :</label><br>
+                                  <label>Intitulé de la formation :</label><br>
+                                  <label>N° stage :</label><br>
+                                  <label>Module :</label><br>
+                                  <label>Prof FAF :</label><br>
+                                  <label>Prof TEL :</label><br>
+                                  <label>Prof TUT :</label><br>
+                                  <label>Période :</label><br>
+                                  <label>Date :</label><br>
+                                  
+                                  </div>
+                                </div>
+                              </div>
+    					</section>
                      
                     </div>
                 </div>
